3dabbb5b0ad80d871224f0887d576223c5bcb599
Joshua Strobl <joshua@stroblindustries.com>
2019-04-09 22:54:59 +0300

    Ensure import can handle directories.

--- src/ferryctl/cmd/import.go
+++ src/ferryctl/cmd/import.go
@@ -22,6 +22,7 @@ import (
 	"libferry"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var importCmd = &cobra.Command{
@@ -33,6 +34,42 @@ var importCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(importCmd)
+}
+
+// GetEopkgs will utilize the provided path to get any eopkgs.
+// If the provided path is a file, we will validate it is an eopkg,. If the provided path is a directory, we'll recursively fetch eopkgs inside its contents.
+func GetEopkgs(providedPath string) (files []string, getErr error) {
+	var pathFile *os.File
+
+	if pathFile, getErr = os.Open(providedPath); getErr == nil { // If we successfully opened the "file"
+		var fileInfo os.FileInfo
+
+		if fileInfo, getErr = pathFile.Stat(); getErr == nil { // If we successfully stat'ed the file
+			if fileInfo.IsDir() { // If this is a directory
+				var nestedNames []string
+
+				if nestedNames, getErr = pathFile.Readdirnames(-1); getErr == nil { // Get all the file contents
+					for _, nestedFileName := range nestedNames { // For each nestedName
+						nestedFiles, nestedErr := GetEopkgs(filepath.Join(providedPath, nestedFileName)) // Get our nested contents
+
+						if nestedErr == nil { // No error
+							files = append(files, nestedFiles...) // Append our nested files
+						} else { // Error happened in nested GetEopkgs
+							getErr = nestedErr
+						}
+					}
+				}
+			} else { // If this is a file
+				if strings.HasSuffix(fileInfo.Name(), "x86_64.eopkg") { // Not a delta
+					files = append(files, providedPath)
+				}
+			}
+		}
+
+		pathFile.Close()
+	}
+
+	return
 }
 
 func importEx(cmd *cobra.Command, args []string) {
@@ -48,15 +85,18 @@ func importEx(cmd *cobra.Command, args []string) {
 	var packages []string
 	for i := 1; i < len(args); i++ {
 		f, err := filepath.Abs(args[i])
+
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to probe: %s: %v\n", f, err)
 			return
 		}
-		if _, err := os.Stat(f); err != nil {
-			fmt.Fprintf(os.Stderr, "File does not exist: %s (%v)\n", f, err)
+
+		if eopkgs, getErr := GetEopkgs(f); getErr == nil { // If we successfully got eopkgs
+			packages = append(packages, eopkgs...)
+		} else {
+			fmt.Fprintf(os.Stderr, "Failed to get eopkgs: %v", getErr)
 			return
 		}
-		packages = append(packages, f)
 	}
 
 	if err := client.ImportPackages(repoID, packages); err != nil {
