2608413560c64f1ad8bdbc24fc66d47986ed0288
Bryan T. Meyers <bmeyers@datadrake.com>
2020-12-30 14:56:04 -0500

    Clean up manifest package

--- manifest/manifest.go
+++ manifest/manifest.go
@@ -34,16 +34,12 @@ const (
 var (
 	// ErrInvalidHeader will be returned when the [manifest] section is malformed
 	ErrInvalidHeader = errors.New("Manifest contains an invalid header")
-
 	// ErrMissingTarget will be returned when the target is not present
 	ErrMissingTarget = errors.New("Manifest contains no target")
-
 	// ErrMissingPayload will be returned when the [[file]]s are missing
 	ErrMissingPayload = errors.New("Manifest does not contain a payload")
-
 	// ErrInvalidPayload will be returned when the payload is in some way invalid
 	ErrInvalidPayload = errors.New("Manifest contains an invalid payload")
-
 	// ErrIllegalUpload is returned when someone is a spanner and tries uploading an unsupported file
 	ErrIllegalUpload = errors.New("The manifest file is NOT an eopkg")
 )
@@ -53,7 +49,6 @@ var (
 type Header struct {
 	// Versioning to protect against future format changes
 	Version string `toml:"version"`
-
 	// The repo that the uploader is intending to upload *to*
 	Target string `toml:"target"`
 }
@@ -63,11 +58,9 @@ type Header struct {
 //
 // This is to ensure all uploads are intentional, complete and verifiable.
 type Manifest struct {
-
 	// Every .tram file has a [manifest] header - this will never change and is
 	// version agnostic.
 	Head Header `toml:"manifest"`
-
 	// A list of files that accompanied this .tram upload
 	File []File `toml:"file"`
 
@@ -82,87 +75,76 @@ func (t *Manifest) ID() string {
 }
 
 // GetPaths will return the package paths as a slice of strings
-func (t *Manifest) GetPaths() []string {
-	var ret []string
-	for i := range t.File {
-		f := &t.File[i]
+func (t *Manifest) GetPaths() (ret []string) {
+	for _, f := range t.File {
 		ret = append(ret, filepath.Join(t.dir, f.Path))
 	}
-	return ret
+	return
 }
 
 // File provides simple verification data for each file in the
 // uploaded payload.
 type File struct {
-
 	// Relative filename, i.e. nano-2.7.5-68-1-x86_64.eopkg
 	Path string `toml:"path"`
-
 	// Cryptographic checksum to allow integrity checks post-upload/pre-merge
 	Sha256 string `toml:"sha256"`
 }
 
 // NewManifest will attempt to load the transit manifest from the
 // named path and perform *basic* validation.
-func NewManifest(path string) (*Manifest, error) {
+func NewManifest(path string) (mf *Manifest, err error) {
 	abs, err := filepath.Abs(path)
 	if err != nil {
-		return nil, err
+		return
 	}
-
-	ret := &Manifest{
+	mf = &Manifest{
 		Path: abs,
 		dir:  filepath.Dir(abs),
 		id:   filepath.Base(abs),
 	}
-
-	blob, err := ioutil.ReadFile(ret.Path)
+	blob, err := ioutil.ReadFile(mf.Path)
 	if err != nil {
-		return nil, err
+		return
 	}
-
-	if _, err := toml.Decode(string(blob), ret); err != nil {
-		return nil, err
+	if _, err = toml.Decode(string(blob), mf); err != nil {
+		return
 	}
-
-	ret.Head.Target = strings.TrimSpace(ret.Head.Target)
-	ret.Head.Version = strings.TrimSpace(ret.Head.Version)
-
-	if ret.Head.Version != "1.0" {
-		return nil, ErrInvalidHeader
+	mf.Head.Target = strings.TrimSpace(mf.Head.Target)
+	mf.Head.Version = strings.TrimSpace(mf.Head.Version)
+	if mf.Head.Version != "1.0" {
+		err = ErrInvalidHeader
+		return
 	}
-
-	if len(ret.Head.Target) < 1 {
-		return nil, ErrMissingTarget
+	if len(mf.Head.Target) == 0 {
+		err = ErrMissingTarget
+		return
 	}
-
-	if len(ret.File) < 1 {
-		return nil, ErrMissingPayload
+	if len(mf.File) == 0 {
+		err = ErrMissingPayload
+		return
 	}
-
-	for i := range ret.File {
-		f := &ret.File[i]
+	for i := range mf.File {
+		f := &mf.File[i]
 		f.Path = strings.TrimSpace(f.Path)
 		f.Sha256 = strings.TrimSpace(f.Sha256)
-
-		if len(f.Path) < 1 || len(f.Sha256) < 1 {
-			return nil, ErrInvalidPayload
+		if len(f.Path) == 0 || len(f.Sha256) == 0 {
+			err = ErrInvalidPayload
+			return
 		}
-
 		if !strings.HasSuffix(f.Path, ".eopkg") {
-			return nil, ErrIllegalUpload
+			err = ErrIllegalUpload
+			return
 		}
 	}
-
-	return ret, nil
+	return
 }
 
 // Verify will verify the files listed in the manifest locally, ensuring
 // that they actually exist, and that the hashes match to prevent any corrupted
 // uploads being inadvertently imported
 func (t *Manifest) Verify() error {
-	for i := range t.File {
-		f := &t.File[i]
+	for _, f := range t.File {
 		path := filepath.Join(t.dir, f.Path)
 		sha, err := core.FileSHA256Sum(path)
 		if err != nil {
